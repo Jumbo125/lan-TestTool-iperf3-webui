4ae28ece8d2dd9306a72384c786cefd722a392b5
Jumbo125 <61667987+Jumbo125@users.noreply.github.com>
2026-02-14 22:39:41 +0100

    Delete static directory

--- static/js/version.js
+++
@@ -1,8 +0,0 @@
-fetch('/iperf_version')
-    .then(response => response.json())
-    .then(data => {
-        document.getElementById('iperf_version').textContent = data.version;
-    })
-    .catch(() => {
-        document.getElementById('iperf_version').textContent = 'Error fetching version';
-    });
